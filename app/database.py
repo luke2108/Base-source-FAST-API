2a80d164bf50ad92abf7ee0e15efcf6ed6a04e76
Luke <luke210855@gmail.com>
2025-01-13 21:47:49 +0800

    update config db

--- app/database.py
+++ app/database.py
@@ -6,7 +6,9 @@ from .config import settings
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=200,
+    encoding='utf-8'
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
